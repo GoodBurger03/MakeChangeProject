ca9a970aa8fe57b18874109521a84379c61715c4
JBartels <busybody0311@gmail.com>
2021-01-15 21:01:49 -0500

    some math. still not printing

--- src/makechange/MakeChange.java
+++ src/makechange/MakeChange.java
@@ -12,26 +12,27 @@ public class MakeChange {
 		int bill10 = 10;
 		int bill5 = 5;
 		int bill1 = 1;
-		double coin25 = 0.25;
-		double coin10 = 0.10;
-		double coin5 = 0.05;
-		double coin1 = 0.01;
+		int coin25 = 25;
+		int coin10 = 10;
+		int coin5 = 5;
+		int coin1 = 1;
 		int change = (int) ((paid * 100) - (price * 100));
-		double finalChange = (double) change / 100;
+		
 		int twenty = change % bill20;
-		double ten = change % bill10;
-		double five = change % bill5;
-		double one = change % bill1;
-		double quart = change % coin25;
-		double dime = change % coin10;
-		double nickle = change % coin5;
-		double penn = change % coin1;
+		int ten = change % bill10;
+		int five = change % bill5;
+		int one = change % bill1;
+		int quart = change % coin25;
+		int dime = change % coin10;
+		int nickle = change % coin5;
+		int penn = change % coin1;
+		double finalChange = ((double) change )/ 100;
 		
 		System.out.print("Hello, enter the price of the item: ");
 		price = kb.nextDouble();
 		System.out.print("How much money did you pay with?: ");
 		paid = kb.nextDouble();
-		kb.close();
+		
 		
 		if(paid < price) {
 			System.out.println("That is not enough, please try again.");
@@ -41,10 +42,41 @@ public class MakeChange {
 		if(paid > price) {
 			System.out.println("Please hold on and I will get your change: " + finalChange);
 			
-			
-		//	if(bill20 == )
-			
+		if(bill20 == (change - twenty)) {
+			System.out.println("You will get a $20 back. ");
+				}
+		if(paid >= ten && paid != twenty) {
+			System.out.println("You will get a $10 back.");
 		}
+		if(paid >= five && paid != ten) {
+			System.out.println("You get a $5 back.");
+		}
+		if(paid >= one && paid != five) {
+			System.out.println("You will get a $1 back.");
+		}
+		if(one >= quart ) {
+			int q = one -quart;
+			q = q / coin25;
+			System.out.println("You will get " +  q + "quarter back");
+		}
+		if(quart >= dime) {
+			int d = quart - dime;
+			d = d /coin10;
+			System.out.println("You will get " + d + "dimes back");
+		}
+		if(dime >= nickle) {
+			int n = dime - nickle;
+			n = n /coin5;
+			System.out.println("You will get " + n + "nickles back");
+		}
+		if(penn >= dime) {
+			int p = penn - dime;
+			p = p / coin1;
+			System.out.println("You will get " + p + "pennies back");
+		}
+		}
+		kb.close();
+		
 				
 			}
 		}
